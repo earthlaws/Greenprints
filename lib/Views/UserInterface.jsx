9253b10ca7e6d48447d784f64093e89949951ef3
Ben Hussey <ben@blip2.net>
2020-08-09 20:07:38 +1000

    base configuration and changes for Greenprints tool (v7)

--- lib/Views/UserInterface.jsx
+++ lib/Views/UserInterface.jsx
@@ -32,10 +32,6 @@ function isBrowserSupportedAV() {
 export default function UserInterface(props) {
   return (
     <StandardUserInterface {...props} version={version}>
-      <Menu>
-        <RelatedMaps viewState={props.viewState} />
-        <MenuItem caption="About" href="about.html" key="about-link" />
-      </Menu>
       <Nav>
         <MeasureTool terria={props.viewState.terria} key="measure-tool" />
       </Nav>
---
+++ runserver.sh
@@ -0,0 +1,1 @@
+./node_modules/.bin/pm2 start ecosystem-production.config.js --update-env --env production
